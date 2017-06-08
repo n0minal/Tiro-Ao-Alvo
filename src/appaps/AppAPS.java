12320a128a575a7c4e12a73612fb9150e5d73856
n0minal <leo_style_lfm@hotmail.com>
2017-06-07 21:14:00 -0300

    Primeiro commit (e talvez último)

---
+++ src/appaps/AppAPS.java
@@ -0,0 +1,23 @@
+/*
+* Alunos: 
+*           Leonardo Ferreira Moura (matrícula 2016103732)
+*           Antonio Carlos de Araujo Testa (matrícula 2016104044)
+*           Guilherme Oliveira de Souza (matrícula 2015101978)
+*           Wilson da Silva Baptista Neto (matrícula 2016200595)
+*           Kauan Cabral Maranhão (matrícula 2016103918)
+*/
+package appaps;
+
+
+public class AppAPS {
+
+    
+    public static void main(String[] args) {
+       //game logic 
+       TiroAoAlvo game = new TiroAoAlvo(100, 5);
+       
+       game.Joga("João");
+       
+    }
+    
+}
---
+++ src/appaps/TiroAoAlvo.java
@@ -0,0 +1,42 @@
+package appaps;
+
+import java.util.Scanner;
+
+public class TiroAoAlvo {
+    Ponto point;
+    
+    //constructor
+    public TiroAoAlvo(int lado, int num){
+        point = new Ponto(lado, num);
+    }
+    
+    public void Joga(String nome){
+        
+        point.GerarPosicaoDoAlvo();
+        
+        System.out.println("====================================================");
+        System.out.println("    Jogador " + nome);
+        System.out.println("    Seu alvo está no intervalo [-" + (point.GetLado()/2) + ", +" + (point.GetLado()/2) + "]");
+        System.out.println("    Você tem direito a " + point.GetTentativas() + " tiros");
+        System.out.println("====================================================");
+        
+        
+        //instanciar classe fora do loop faz bem
+        Scanner scan = new Scanner (System.in);
+        
+        for(int i = 0; i < point.GetTentativas(); i++){
+            System.out.println("*** Tiro " + point.GetTiroAtual() + " — entre com o par x y:");
+
+            int tiro_x = scan.nextInt();
+            int tiro_y = scan.nextInt();
+                    
+            int distance = point.GetDistanciaEntrePontos(tiro_x, tiro_y, point.GetX(), point.GetY());
+            int score = point.CalcularPontuacao(distance);
+            point.SetTiroAtual(point.GetTiroAtual() + 1);
+            System.out.println("Você fez " + score + " pontos!");
+            point.SetParcial(point.GetParcial() + score);
+            System.out.println("Sua parcial agora é " + point.GetParcial());            
+        }
+        System.out.println("Fim das tentativas cowboy... Você fez um total de " + point.GetParcial() + " pontos, parabéns!");
+    }
+}
